d348bf8f1426b861d097cd8ba8fdcd842e9a956c
Dhanushka Samarakoon <99551588+DsamarakooN@users.noreply.github.com>
2023-01-22 23:03:13 +0530

    initial commit

---
+++ schemas/pin.js
@@ -0,0 +1,12 @@
+export default {
+    name: 'pin',
+    title: 'pin',
+    type: 'document',
+    fields: [
+        {
+            name : 'title',
+            title: 'title',
+            type: 'string',
+        },
+    ]
+}
